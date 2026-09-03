0da893a3b9ab4fe49ab44d01009bca28be6bc638
Biraj Pandey <bpandey@uw.edu>
2026-09-03 07:32:44 -0700

    Solve transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -6,8 +6,8 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     """
     Compute scaled dot-product attention.
     """
-    d_k = K.shape[-1]
-    scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
-    attention_weights = F.softmax(scores, dim=-1)
-    output = torch.matmul(attention_weights, V)
-    return output 
+    d_k = Q.shape[-1]
+    h = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
+    alpha = F.softmax(h, dim=-1)
+    output = torch.matmul(alpha, V)
+    return output
